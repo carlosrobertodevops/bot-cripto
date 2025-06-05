e0559ace7823b2a4f0eb01fb4b02771d43053dec
“carlosrobertodevops” <carlos.roberto.devops@gmail.com>
2025-06-05 19:50:38 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+
+const axios = require("axios");
+
+const SYMBOL = "BTCUSDT";
+const BUY_PRICE = 101554.108;
+const SELL_PRICE = 10433.05;
+
+const API_URL = "https://testnet.binance.vision";
+
+async function start() {
+  const { data } = await axios.get(API_URL + `/api/v3/klines?limit=21&interval=15m&symbol=${SYMBOL}`);
+  const candle = data[data.length - 1];
+  console.log(candle);
+}
+
+setInterval(start, 3000);
+
+start();
