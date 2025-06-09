3f22ed75c678e771c7af2aa3106e1596b390c0c1
Carlos Roberto <carlos.roberto.devops@gmail.com>
2025-06-09 17:31:06 -0300

    Novas aulas

--- index1.js
+++ index1.js
@@ -2,14 +2,16 @@
 const axios = require("axios");
 
 const SYMBOL = "BTCUSDT";
-const BUY_PRICE = 101700.00;
-const SELL_PRICE = 101619.05;
+const BUY_PRICE = 108000.00;
+const SELL_PRICE = 108500.00;
 
 const API_URL = "https://testnet.binance.vision";
 
 let isOpened = false;
 
+
 async function start() {
+  // Obtener los últimos 21 datos de 15 minutos
   const { data } = await axios.get(API_URL + `/api/v3/klines?limit=21&interval=15m&symbol=${SYMBOL}`);
   const candle = data[data.length - 1];
   const price = parseFloat(candle[4]);
@@ -17,7 +19,10 @@ async function start() {
   console.clear();
   console.log('Price: ', price);
 
-  if (price <= BUY_PRICE && !isOpened) {
+
+  console.log('Is Opened? ', isOpened);
+
+  if (price <= BUY_PRICE  && !isOpened) {
     console.log('Comprar');
     isOpened = true;
   }
@@ -27,7 +32,6 @@ async function start() {
   }
   else
     console.log('Esperar');
-
 }
 
 setInterval(start, 3000);
---
+++ index22.js
@@ -0,0 +1,49 @@
+
+const axios = require("axios");
+
+const SYMBOL = "BTCUSDT";
+const BUY_PRICE = 108000.00;
+const SELL_PRICE = 108500.00;
+
+const API_URL = "https://testnet.binance.vision";
+
+let isOpened = false;
+
+// Calculo de SMA (Simple Moving Average)
+function calcSMA(data) {
+  const closes = data.map(candle => parseFloat(candle[4]));
+  const sum = closes.reduce((a, b) => a + b);
+  return sum / data.length;
+}
+
+async function start() {
+  // Obtener los últimos 21 datos de 15 minutos
+  const { data } = await axios.get(API_URL + `/api/v3/klines?limit=21&interval=15m&symbol=${SYMBOL}`);
+  const candle = data[data.length - 1];
+  const price = parseFloat(candle[4]);
+
+  console.clear();
+  console.log('Price: ', price);
+
+  const sma21 = calcSMA(data);
+  console.log('SMA (21): ', sma21);
+
+  console.log('Is Opened? ', isOpened);
+
+  // Preco 10% mais barato 
+  if (price <= (sma21 * 0.9) && !isOpened) {
+    console.log('Comprar');
+    isOpened = true;
+  }
+  // Vender com preço 10% mais caro
+  else if (price >= (sma21 * 1.1) && isOpened) {
+    console.log('Vender');
+    isOpened = false;
+  }
+  else
+    console.log('Esperar');
+}
+
+setInterval(start, 3000);
+
+start();
